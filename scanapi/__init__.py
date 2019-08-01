11916c35148cfb5791528f8dc1c394715749035f
Camila Maia <cmaiacd@gmail.com>
2019-08-01 16:03:27 -0300

    APINode and RequestNode

--- scanapi/__init__.py
+++ scanapi/__init__.py
@@ -8,16 +8,18 @@ import click
 
 
 @click.command()
-@click.option('-s', '--spec-path', 'spec_path', type=click.Path(exists=True), default=SETTINGS['spec_path'])
-@click.option('-d', '--docs-path', 'docs_path', default=SETTINGS['docs_path'])
+@click.option(
+    "-s",
+    "--spec-path",
+    "spec_path",
+    type=click.Path(exists=True),
+    default=SETTINGS["spec_path"],
+)
+@click.option("-d", "--docs-path", "docs_path", default=SETTINGS["docs_path"])
 def scan(spec_path, docs_path):
     """Automated Testing and Documentation for your REST API."""
 
-    SETTINGS.update({'spec_path': spec_path, 'docs_path': docs_path})
+    SETTINGS.update({"spec_path": spec_path, "docs_path": docs_path})
 
-    responses = RequestsBuilder(spec_path).all_responses()
+    responses = RequestsBuilder(spec_path).call_all()
     DocsWriter(docs_path).write(responses)
-
-
-if __name__ == "__main__":
-    scan()
