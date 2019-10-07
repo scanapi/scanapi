e7766d33b77504e614b7a2c5b3f25134cb1a6fb2
Camila Maia <cmaiacd@gmail.com>
2019-10-07 14:11:06 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scanapi",
-    version="0.0.12",
+    version="0.0.13",
     author="Camila Maia",
     author_email="cmaiacd@gmail.com",
     description="Automated Testing and Documentation for your REST API",
