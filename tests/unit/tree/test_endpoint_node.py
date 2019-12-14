e7530058d45bfb3c13ff6d9c2743d43c1e0c2b15
Camila Maia <cmaiacd@gmail.com>
2019-12-14 07:55:03 -0300

    Fix join of urls and add tests for it

---
+++ tests/unit/tree/test_endpoint_node.py
@@ -0,0 +1,37 @@
+import pytest
+from scanapi.tree.endpoint_node import join_urls
+
+
+class TestJoinUrls:
+    test_data = [
+        (
+            "http://demo.scanapi.dev/api/",
+            "health",
+            "http://demo.scanapi.dev/api/health",
+        ),
+        (
+            "http://demo.scanapi.dev/api/",
+            "/health",
+            "http://demo.scanapi.dev/api/health",
+        ),
+        (
+            "http://demo.scanapi.dev/api/",
+            "/health/",
+            "http://demo.scanapi.dev/api/health/",
+        ),
+        ("http://demo.scanapi.dev/api", "health", "http://demo.scanapi.dev/api/health"),
+        (
+            "http://demo.scanapi.dev/api",
+            "/health",
+            "http://demo.scanapi.dev/api/health",
+        ),
+        (
+            "http://demo.scanapi.dev/api",
+            "/health/",
+            "http://demo.scanapi.dev/api/health/",
+        ),
+    ]
+
+    @pytest.mark.parametrize("url_1, url_2, expected", test_data)
+    def test_build_url_properly(self, url_1, url_2, expected):
+        assert join_urls(url_1, url_2) == expected
