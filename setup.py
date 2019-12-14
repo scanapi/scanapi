34991b691036cb5f8b2dc284206bd55fbc950b68
Camila Maia <cmaiacd@gmail.com>
2019-12-14 11:36:16 -0300

    Upgrade version on setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scanapi",
-    version="0.0.14",
+    version="0.0.15",
     author="Camila Maia",
     author_email="cmaiacd@gmail.com",
     description="Automated Testing and Documentation for your REST API",
