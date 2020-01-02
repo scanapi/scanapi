86e89e6ab52bbf64e058c02dbfdbbb1500066bff
Camila Maia <cmaiacd@gmail.com>
2020-01-02 19:33:30 -0300

    Release v0.0.18

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scanapi",
-    version="0.0.17",
+    version="0.0.18",
     author="Camila Maia",
     author_email="cmaiacd@gmail.com",
     description="Automated Testing and Documentation for your REST API",
