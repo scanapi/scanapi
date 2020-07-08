cfe1762cd16193d5aaba989f82613a1f37fa7b3a
Matheus Abreu <abreumatheus@icloud.com>
2020-07-08 00:46:32 -0300

    Failing test test_should_call_openapi_to_yaml

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,22 +1,34 @@
 from click.testing import CliRunner
 
-from scanapi.main import run
+from scanapi.main import run, convert
 
 
-class TestRun:
-    def test_call_save_preferences(self, mocker):
-        runner = CliRunner()
-        mock_save_preferences = mocker.patch(
-            "scanapi.settings.Settings.save_preferences"
-        )
-        result = runner.invoke(run, ["--output-path", "my_output.html"])
+class TestMain:
+    class TestRun:
+        def test_call_save_preferences(self, mocker):
+            runner = CliRunner()
+            mock_save_preferences = mocker.patch(
+                "scanapi.settings.Settings.save_preferences"
+            )
+            result = runner.invoke(run, ["--output-path", "my_output.html"])
 
-        assert result.exit_code == 4
-        mock_save_preferences.assert_called_once_with(
-            **{
-                "spec_path": None,
-                "output_path": "my_output.html",
-                "config_path": None,
-                "template": None,
-            }
-        )
+            assert result.exit_code == 4
+            mock_save_preferences.assert_called_once_with(
+                **{
+                    "spec_path": None,
+                    "output_path": "my_output.html",
+                    "config_path": None,
+                    "template": None,
+                }
+            )
+
+    class TestConvert:
+        def test_should_call_openapi_to_yaml(self, mocker):
+            runner = CliRunner()
+            mock_openapi_to_yaml = mocker.patch("scanapi.main.openapi_to_yaml")
+            result = runner.invoke(convert, '../data/openapi.json')
+
+            assert result.exit_code == 0
+
+            mock_openapi_to_yaml.assert_called_once_with('../data/openapi.json')
+
