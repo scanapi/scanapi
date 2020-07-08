d513ef74c3fa4b4f66d9d1479631692d1d4153c6
Matheus Abreu <abreumatheus@icloud.com>
2020-07-08 00:39:57 -0300

    Added extra verifications to script

--- scanapi/openapi_to_yaml.py
+++ scanapi/openapi_to_yaml.py
@@ -1,5 +1,24 @@
 import json
+import os
+
 import yaml
+
+from .errors import EmptyOpenAPIFileError, InvalidOpenAPIFileError
+
+
+def _is_file_empty(file_path):
+    if not os.stat(file_path).st_size:
+        raise EmptyOpenAPIFileError(file_path)
+
+
+def _is_valid_openapi_spec(open_api_dict: dict, json_file_path: str):
+
+    required_general_keys = ['info', 'host', 'schemes', 'basePath', 'paths']
+
+    all_general_keys = all(key in open_api_dict for key in required_general_keys)
+
+    if not all_general_keys or 'title' not in open_api_dict['info']:
+        raise InvalidOpenAPIFileError(json_file_path)
 
 
 def openapi_to_yaml(json_file_path: str):
@@ -8,8 +27,12 @@ def openapi_to_yaml(json_file_path: str):
     :param json_file_path: Path to the OpenAPI File
     :return: None
     """
+    _is_file_empty(json_file_path)
+
     with open(json_file_path, 'r') as file:
         openapi_specs = json.load(file)
+
+    _is_valid_openapi_spec(openapi_specs, json_file_path)
 
     base_yaml = {"api":
                      {"endpoints":
