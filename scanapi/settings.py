4f116e2e3250088437733fa717c439b1f67497c0
supakeen <cmdr@supakeen.com>
2020-07-25 18:10:09 +0200

    Don't merge files, use the most specific one.

--- scanapi/settings.py
+++ scanapi/settings.py
@@ -4,10 +4,7 @@ import appdirs
 from scanapi.config_loader import load_config_file
 
 
-GLOBAL_CONFIG_PATH = os.path.join(
-    appdirs.site_config_dir("scanapi"),
-    "scanapi.conf",
-)
+GLOBAL_CONFIG_PATH = os.path.join(appdirs.site_config_dir("scanapi"), "scanapi.conf",)
 
 LOCAL_CONFIG_PATH = "./scanapi.conf"
 
@@ -25,21 +22,15 @@ class Settings(dict):
 
     def save_config_file_preferences(self, config_path=None):
         """ Saves the Settings object config file preferences. """
-        if not config_path and not self.has_local_config_file:
-            return
-
-        if os.path.isfile(GLOBAL_CONFIG_PATH):
-            global_config = load_config_file(GLOBAL_CONFIG_PATH)
-            self.update(**global_config)
-
-        if not config_path:
-            user_config = load_config_file(LOCAL_CONFIG_PATH)
+        if config_path:
+            self["config_path"] = config_path
+            self.update(**load_config_file(config_path))
+        elif self.has_local_config_file:
             self["config_path"] = LOCAL_CONFIG_PATH
-            self.update(**user_config)
-            return
-
-        user_config = load_config_file(config_path)
-        self.update(**user_config)
+            self.update(**load_config_file(LOCAL_CONFIG_PATH))
+        elif self.has_global_config_file:
+            self["config_path"] = GLOBAL_CONFIG_PATH
+            self.update(**load_config_file(GLOBAL_CONFIG_PATH))
 
     def save_click_preferences(self, **preferences):
         """ Saves all preference items to the Settings object. """
