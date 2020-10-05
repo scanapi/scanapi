41152eb8f035e63b78c1261adcef13727961ca61
Marcus Pereira <mpereirassa@gmail.com>
2020-10-05 16:20:07 -0300

    .fix name

--- tests/unit/test_tools.py
+++ tests/unit/test_tools.py
@@ -1,7 +1,7 @@
 import httpx
 import pytest
 
-from scanapi.tools.curl import request_to_curl
+from scanapi.tools.curl import convert_httpx_request_to_curl
 
 @pytest.mark.asyncio
 async def test_something_async(httpx_mock):
@@ -9,7 +9,7 @@ async def test_something_async(httpx_mock):
 
     async with httpx.AsyncClient() as client:
         response = await client.get("http://pokemon/1")
-        result = request_to_curl(response.request)
+        result = convert_httpx_request_to_curl(response.request)
         assert result == ('curl -X GET -H "host: pokemon" -H "accept: */*" -H "accept-encoding: gzip, '
  'deflate" -H "connection: keep-alive" -H "user-agent: python-httpx/0.15.5" -d '
  "'' http://pokemon/1 --compressed")
