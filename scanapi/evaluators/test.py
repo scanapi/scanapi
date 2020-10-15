e8c8fecc3dee98322818b8e8d4799df7efd50434
vjern <jocelyn.vernay@gmail.com>
2020-10-15 23:46:03 +0200

    Add remote method call implementation with inplace import path resolution

---
+++ scanapi/evaluators/test.py
@@ -0,0 +1,20 @@
+def it(a: int, b: int = 0) -> int:
+    return a + b
+
+it.it = it
+
+
+def off(a: int, b: int, status: str = 'x') -> int:
+    return str(a) + '+' + str(b) + '-' + status
+
+
+class response:
+
+    def ok(response):
+        return response.status < 400
+
+    # def status_is(response, code: int):
+    #     return response.status == code
+
+    def status_is(code: int, response):
+        return response.status == code
