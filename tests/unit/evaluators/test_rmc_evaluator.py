e2243e966d2a0abd8e694c9ffdcf7fe460aaf018
zebralt <jocelyn.vernay@gmail.com>
2020-10-23 18:53:17 +0200

    Refactor rmc_evaluator after code_evaluator

--- tests/unit/evaluators/test_rmc_evaluator.py
+++ tests/unit/evaluators/test_rmc_evaluator.py
@@ -1,12 +1,20 @@
+import ast
 
-from scanapi.evaluators.rmc import remote_method_call, get_module
+from scanapi.evaluators.rmc_evaluator import RemoteMethodCallEvaluator, get_module, unroll_name
 
 from unittest.mock import Mock
 
 
+def test_unroll_name():
+
+    assert unroll_name(ast.parse('a', mode='eval').body) == 'a'
+    assert unroll_name(ast.parse('a.c', mode='eval').body) == 'a.c'
+    assert unroll_name(ast.parse('a.b.c', mode='eval').body) == 'a.b.c'
+
+
 def test_rmc():
 
-    rmc = remote_method_call
+    rmc = RemoteMethodCallEvaluator.evaluate
 
     # bare name
     assert rmc('scanapi.std:response.ok', {'response': Mock(status_code=400)}) == False
@@ -23,7 +31,7 @@ def test_rmc():
 
 def test_std():
 
-    rmc = remote_method_call
+    rmc = RemoteMethodCallEvaluator.evaluate
 
     assert rmc('std:response.ok', {'response': Mock(status_code=400)}) == False
     assert rmc(':response.ok', {'response': Mock(status_code=400)}) == False
