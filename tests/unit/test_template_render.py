d8ae706af64d6f0daaa6d2dd6ebd04485af4bb8f
beatorizu <betrizaf@gmail.com>
2020-12-13 19:41:45 -0300

    Format render tests

--- tests/unit/test_template_render.py
+++ tests/unit/test_template_render.py
@@ -24,20 +24,19 @@ class TestTemplateRender:
         def test_should_render_json(self, mocked__request):
             request = mocked__request()
             request.headers = {
-                "Content-Length": "138",
+                "Content-Length": "44",
                 "Content-Type": "application/json",
             }
-            request.body = b'{"uuid": "76fe526d665a46f2812ec6580dd5b34a", "name": "Tarik", "yearsOfExperience": 2, "languages": ["ruby go"], "newOpportunities": false}'
-            assert (
-                '{"uuid": "76fe526d665a46f2812ec6580dd5b34a", "name": "Tarik", "yearsOfExperience": 2, "languages": ["ruby go"], "newOpportunities": false}'
-                == render_body(request)
+            request.body = b'{"uuid": "76fe526d665a46f2812ec6580dd5b34a"}'
+            assert '{"uuid": "76fe526d665a46f2812ec6580dd5b34a"}' == render_body(
+                request
             )
 
         def test_should_render_plain_text(self, mocked__request):
             request = mocked__request()
             request.headers = {"Content-Length": "35", "Content-Type": "text/plain"}
-            request.body = b"this is a custom plain text message"
-            assert "this is a custom plain text message" == render_body(request)
+            request.body = b"this is a custom plain text"
+            assert "this is a custom plain text" == render_body(request)
 
         def test_should_render_binary_content(self, mocked__request):
             request = mocked__request()
