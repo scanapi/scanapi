ac144284fde03d64317f9ab5aa0d898b1daf05bf
Matheus Abreu <matheus.abreu@olist.com>
2020-12-22 14:09:19 -0300

    Added OpenApi spec validation and Json validation

--- scanapi/openapi_to_yaml.py
+++ scanapi/openapi_to_yaml.py
@@ -2,23 +2,55 @@ import json
 import os
 
 import yaml
+from openapi_spec_validator import validate_spec, validate_v2_spec
+from openapi_spec_validator.exceptions import OpenAPIValidationError
 
-from scanapi.errors import EmptyOpenAPIFileError, InvalidOpenAPIFileError
+from scanapi.errors import (
+    EmptyJsonFileError,
+    InvalidJsonFileError,
+    InvalidOpenAPISpecError,
+    NotAJsonFileError,
+)
 
 
-def _is_file_empty(file_path):
-    if not os.stat(file_path).st_size:
-        raise EmptyOpenAPIFileError(file_path)
+def _is_json_file(file_path: str) -> bool:
+    if file_path.lower().endswith(".json"):
+        return True
+    raise NotAJsonFileError(file_path)
 
 
-def _is_valid_openapi_spec(open_api_dict: dict, json_file_path: str):
+def _is_json_file_empty(file_path: str) -> bool:
+    if os.stat(file_path).st_size:
+        return False
+    raise EmptyJsonFileError(file_path)
 
-    required_general_keys = ["info", "host", "schemes", "basePath", "paths"]
 
-    all_general_keys = all(key in open_api_dict for key in required_general_keys)
+def _validate_json_file(file_path):
+    if _is_json_file(file_path) and not _is_json_file_empty(file_path):
+        try:
+            with open(file_path, "r") as file:
+                openapi_specs = json.load(file)
+            return openapi_specs
+        except ValueError:
+            pass
 
-    if not all_general_keys or "title" not in open_api_dict["info"]:
-        raise InvalidOpenAPIFileError(json_file_path)
+        raise InvalidJsonFileError
+
+
+def _validate_openapi_spec(open_api_dict: dict):
+    try:
+        validate_spec(open_api_dict)
+        return True
+    except OpenAPIValidationError:
+        pass
+
+    try:
+        validate_v2_spec(open_api_dict)
+        return True
+    except OpenAPIValidationError:
+        pass
+
+    raise InvalidOpenAPISpecError
 
 
 def openapi_to_yaml(json_file_path: str):
@@ -27,14 +59,13 @@ def openapi_to_yaml(json_file_path: str):
     :param json_file_path: Path to the OpenAPI File
     :return: None
     """
-    _is_file_empty(json_file_path)
+    openapi_specs = _validate_json_file(json_file_path)
 
-    with open(json_file_path, "r") as file:
-        openapi_specs = json.load(file)
+    _validate_openapi_spec(openapi_specs)
 
-    _is_valid_openapi_spec(openapi_specs, json_file_path)
-
-    base_yaml = {"api": {"endpoints": [{"name": None, "path": None, "requests": []}]}}
+    base_yaml = {
+        "api": {"endpoints": [{"name": None, "path": None, "requests": []}]}
+    }
 
     endpoints = base_yaml["api"]["endpoints"][0]
     requests = endpoints["requests"]
@@ -58,5 +89,7 @@ def openapi_to_yaml(json_file_path: str):
 
     with open("./api.yaml", "w") as file:
         file.write(
-            yaml.dump(base_yaml, default_flow_style=False, sort_keys=False, indent=4)
+            yaml.dump(
+                base_yaml, default_flow_style=False, sort_keys=False, indent=4
+            )
         )
