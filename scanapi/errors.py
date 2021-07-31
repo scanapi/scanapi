e004b63cd03e9dff2385d648dbedec062191c671
Don Smith <37778580+djs-djs@users.noreply.github.com>
2021-07-31 07:50:31 -0700

    Update doc line for length according to style guide

--- scanapi/errors.py
+++ scanapi/errors.py
@@ -40,7 +40,8 @@ class InvalidPythonCodeError(MalformedSpecError):
 
 
 class BadConfigurationError(Exception):
-    """Raised when an environment variable was not set or was badly configured"""
+    """Raised when an environment variable was not set 
+    or was badly configured"""
 
     def __init__(self, env_var, *args):
         super(BadConfigurationError, self).__init__(
