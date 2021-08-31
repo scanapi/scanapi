204e73aa17e1d5c1d7672de41c69dc3ddcdc0b19
João Gabriel Trombeta <joaog.trombeta@gmail.com>
2021-08-31 12:54:12 -0700

    docs: add docstring to endpoint_node (#505)

--- scanapi/tree/endpoint_node.py
+++ scanapi/tree/endpoint_node.py
@@ -22,6 +22,18 @@ logger = logging.getLogger(__name__)
 
 
 class EndpointNode:
+    """
+    Class that represents an endpoint. It follows a tree-like structure
+    where each EndpointNode may contain multiple children EndpointNodes.
+
+    Attributes:
+        spec[dict]: dictionary containing the endpoint's specifications
+        parent[EndpointNode, optional]: the parent node
+        child_nodes[list of EndpointNodes]: the children nodes
+        spec_vars[SpecEvaluator]: evaluator used to evaluate expressions
+                                  and store spec variables
+    """
+
     SCOPE = "endpoint"
     ALLOWED_KEYS = (
         ENDPOINTS_KEY,
@@ -44,6 +56,9 @@ class EndpointNode:
         self.spec_vars = SpecEvaluator(self, spec.get(VARS_KEY, {}))
 
     def __build(self):
+        """Validate the EndpointNode keys and create children EndpointNodes
+        from endpoints in its specifications.
+        """
         self._validate()
 
         self.child_nodes = [
@@ -56,6 +71,12 @@ class EndpointNode:
 
     @property
     def name(self):
+        """Get the endpoint's name. The name is prepended by the parent's name,
+        if it is not a root node.
+
+        Returns:
+            [str]: The endpoint's name.
+        """
         name = self.spec.get(NAME_KEY, "")
 
         if self.is_root or not self.parent.name:
@@ -65,6 +86,13 @@ class EndpointNode:
 
     @property
     def path(self):
+        """Get the endpoint's path. The path is prepended by the parent's path,
+        if it is not a root node. The returned path already has all variables
+        evaluated.
+
+        Returns:
+            [str]: The endpoint's url.
+        """
         path = str(self.spec.get(PATH_KEY, "")).strip()
         url = join_urls(self.parent.path, path) if self.parent else path
 
@@ -72,22 +100,50 @@ class EndpointNode:
 
     @property
     def headers(self):
+        """Get the headers used in the endpoint call. The headers of the
+        call include the parent's headers.
+
+        Returns:
+            [dict]: the headers used in the endpoint call.
+        """
         return self._get_specs(HEADERS_KEY)
 
     @property
     def params(self):
+        """Get the parameters used in the endpoint call. The parameters of the
+        call include the parent's parameters.
+
+        Returns:
+            [dict]: the parameters used in the endpoint call.
+        """
         return self._get_specs(PARAMS_KEY)
 
     @property
     def delay(self):
+        """Get the time in milliseconds to be waited before making the endpoint
+        call.
+
+        Returns:
+            [int]: the time to be waited.
+        """
         delay = self.spec.get(DELAY_KEY, 0)
         return delay or getattr(self.parent, DELAY_KEY, 0)
 
     @property
     def is_root(self):
+        """Check if the EndpointNode is a root node.
+
+        Returns:
+            [bool]: true if the node has no parent, false otherwise.
+        """
         return not self.parent
 
     def run(self):
+        """Run the requests of the node and all children nodes.
+
+        Returns:
+            [iterator]: Iterator that yields the test result of each request.
+        """
         for request in self._get_requests():
             try:
                 yield request.run()
@@ -109,6 +165,14 @@ class EndpointNode:
         validate_keys(self.spec.keys(), self.ALLOWED_KEYS, required_keys, scope)
 
     def _get_specs(self, field_name):
+        """Get a specification of the endpoint.
+
+        Args:
+            field_name [str]: name of the specification field.
+
+        Returns:
+            [dict]: a dictionary containing the values of the field.
+        """
         values = self.spec.get(field_name, {})
         parent_values = getattr(self.parent, field_name, None)
 
@@ -118,6 +182,12 @@ class EndpointNode:
         return values
 
     def _get_requests(self):
+        """Get all requests from the node and children nodes as RequestNodes.
+
+        Returns:
+            [iterator]: Iterator that yields a RequestNode for
+            each request.
+        """
         return chain(
             (
                 RequestNode(spec, self)
