2cd4d9d180e4bcba9935f4ac9fbdd5d7f4825473
guites <gui.garcia67@gmail.com>
2026-03-05 12:02:36 -0300

    test: test the convert function

---
+++ tests/unit/test_convert.py
@@ -0,0 +1,146 @@
+from scanapi.convert import convert
+import logging
+from pytest import mark, raises
+
+
+@mark.describe("convert")
+@mark.describe("convert")
+class TestConvert:
+    @mark.context("when called with an openapi_path")
+    @mark.it("should call the resolving parser on the openapi_path")
+    def test_call_resolving_parser_on_path(self, mocker):
+        parser_mock = mocker.Mock()
+
+        parser_mock.parse.return_value = None
+        parser_mock.specification = {"openapi": "3.0.4"}
+
+        mock_resolving_parser = mocker.patch(
+            "scanapi.convert.prance.ResolvingParser", return_value=parser_mock
+        )
+
+        mocker.patch("scanapi.convert.yaml.dump")
+        mocker.patch("builtins.open", mocker.mock_open())
+
+        convert("openapi.json", "$BASE_URL", "scanapi.yaml")
+        mock_resolving_parser.assert_called_once_with("openapi.json")
+
+    @mark.context("when the convertion runs successfully")
+    @mark.it("should write to the output_path")
+    def test_writes_to_output_path(self, mocker):
+        parser_mock = mocker.Mock()
+
+        parser_mock.parse.return_value = None
+        parser_mock.specification = {"openapi": "3.0.4"}
+
+        mocker.patch(
+            "scanapi.convert.prance.ResolvingParser", return_value=parser_mock
+        )
+        converter_mock = mocker.Mock()
+        converter_mock.convert.return_value = {"test": "test"}, set()
+
+        mocker.patch(
+            "scanapi.convert.OpenAPIConverter", return_value=converter_mock
+        )
+
+        yaml_dump_mock = mocker.patch("scanapi.convert.yaml.dump")
+        open_mock = mocker.patch("builtins.open", mocker.mock_open())
+
+        convert("openapi.json", "$BASE_URL", "test.yaml")
+        open_mock.assert_called_once_with("test.yaml", "w")
+        yaml_dump_mock.assert_called_once_with(
+            {"test": "test"},
+            open_mock(),
+            default_flow_style=False,
+            sort_keys=False,
+            indent=4,
+        )
+
+    @mark.context("when the convertion generates created_variables")
+    @mark.it("should print the variables to the console")
+    def test_prints_created_variables(self, mocker, capsys):
+        parser_mock = mocker.Mock()
+
+        parser_mock.parse.return_value = None
+        parser_mock.specification = {"openapi": "3.0.4"}
+
+        mocker.patch(
+            "scanapi.convert.prance.ResolvingParser", return_value=parser_mock
+        )
+        converter_mock = mocker.Mock()
+        converter_mock.convert.return_value = (
+            {"test": "test"},
+            {"one", "two", "three"},
+        )
+
+        mocker.patch(
+            "scanapi.convert.OpenAPIConverter", return_value=converter_mock
+        )
+
+        mocker.patch("scanapi.convert.yaml.dump")
+        mocker.patch("builtins.open", mocker.mock_open())
+
+        convert("openapi.json", "$BASE_URL", "test.yaml")
+        captured = capsys.readouterr()
+
+        assert "The following variables were created" in captured.out
+        assert "${one}" in captured.out
+        assert "${two}" in captured.out
+        assert "${three}" in captured.out
+
+    @mark.context("when the openapi_path points to an invalid yaml file")
+    @mark.it("should log an error")
+    def test_handles_invalid_yaml_file(self, caplog):
+        with caplog.at_level(logging.ERROR):
+            with raises(SystemExit) as excinfo:
+                convert(
+                    "tests/data/convert/invalid.yaml",
+                    "$BASE_URL",
+                    "scanapi.yaml",
+                )
+
+            assert excinfo.type == SystemExit
+            assert excinfo.value.code == 1
+
+    @mark.context("when the openapi_path points to an invalid json file")
+    @mark.it("should log an error")
+    def test_handles_invalid_json_file(self, caplog):
+        with caplog.at_level(logging.ERROR):
+            with raises(SystemExit) as excinfo:
+                convert(
+                    "tests/data/convert/invalid.json",
+                    "$BASE_URL",
+                    "scanapi.yaml",
+                )
+
+            assert excinfo.type == SystemExit
+            assert excinfo.value.code == 1
+
+    @mark.context("when the openapi_path points to an empty json file")
+    @mark.it("should log an error")
+    def test_handles_empty_json_file(self, caplog):
+        with caplog.at_level(logging.ERROR):
+            with raises(SystemExit) as excinfo:
+                convert(
+                    "tests/data/convert/empty.json", "$BASE_URL", "scanapi.yaml"
+                )
+
+            assert excinfo.type == SystemExit
+            assert excinfo.value.code == 1
+
+    @mark.context(
+        "when the openapi_path points to a schema without version definition"
+    )
+    @mark.it("should log an error")
+    def test_handles_invalid_openapi_schema(self, caplog):
+        with caplog.at_level(logging.ERROR):
+            with raises(SystemExit) as excinfo:
+                convert(
+                    "tests/data/convert/no_version_definition.yaml",
+                    "$BASE_URL",
+                    "scanapi.yaml",
+                )
+
+            assert excinfo.type == SystemExit
+            assert excinfo.value.code == 1
+
+            assert "Couldn't parse OpenAPI schema" in caplog.text
